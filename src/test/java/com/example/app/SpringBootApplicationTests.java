61121748d2b983de87a76fc0ef3948fffdb1b76a
Mudawikawi Grahadhuita <mgrahadhuita@traveloka.com>
2024-09-05 16:51:47 +0700

    small refactor

--- src/test/java/com/example/app/SpringBootApplicationTests.java
+++ src/test/java/com/example/app/SpringBootApplicationTests.java
@@ -1,10 +1,10 @@
-package com.grhamuda.customredissoncachemanager;
+package com.example.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomRedissonCacheManagerApplicationTests {
+class SpringBootApplicationTests {
 
   @Test
   void contextLoads() {
