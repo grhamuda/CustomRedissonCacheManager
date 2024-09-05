cc98fc68648ac8bfcbce93401e04ebdbc284e68b
Mudawikawi Grahadhuita <mgrahadhuita@traveloka.com>
2024-09-05 16:49:36 +0700

    initial commit

---
+++ src/main/java/com/grhamuda/customredissoncachemanager/CustomRedissonCacheManagerApplication.java
@@ -0,0 +1,13 @@
+package com.grhamuda.customredissoncachemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomRedissonCacheManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CustomRedissonCacheManagerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/grhamuda/customredissoncachemanager/CustomRedissonCacheManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grhamuda.customredissoncachemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomRedissonCacheManagerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
